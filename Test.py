c3f0677d7108c8900d202883f73d8e935b2ddf50
rajay458 <rajashekary458@gmail.com>
2016-12-07 14:47:20 +0530

    Created file and uploaded

---
+++ Test.py
@@ -0,0 +1,11 @@
+class test:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age 
+        print(self.name)
+        print(self.age)
+    def test (self):
+        print("hello world")
+        
+testObj = test("raja",27)
+testObj.test()
